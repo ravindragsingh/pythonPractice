36a4d4fdd885e9809ddb6b1a04be2a7eb85e16be
Pratapsingh <ravindra.pratapsingh@accenture.com>
2024-10-10 14:27:12 -0400

    calculating volume function

--- practice.py
+++ practice.py
@@ -3,11 +3,23 @@
 print(len(name))
 '''
 
-#functions
-
+'''
+#functions def is the keyuword for defining a function
 def find_total(test):
     print(test)
 test = "First function"
 hello = find_total(test)
+'''
 
+#function to print the volume of a cylinder 
 
+def find_cylinder_volum(radius,height):
+    
+    return 3.14*(radius**2)*height
+
+r = 5
+h = 12
+
+print(find_cylinder_volum(r,h))
+
+ 
