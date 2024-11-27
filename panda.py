59c780f69a4a06758f801c93349188c0c3343db8
Pratapsingh <ravindra.pratapsingh@accenture.com>
2024-11-27 14:38:11 -0500

    pandaFirstupdated

--- panda.py
+++ panda.py
@@ -10,3 +10,10 @@ df = pd.read_csv("C:/Users/ravindra.pratapsingh/Downloads/movies.csv")
 print(df.head(1))
 print(df.imdb_rating.min())
 print(df.imdb_rating.mean())
+bollywoodMovies = df[df.industry == "Bollywood"]  ## creating a new data frame for bollywood movies only
+hollywoodMovies = df[df.industry == "Hollywood"]  ## creating a new data frame for hollywood movies only
+
+print(hollywoodMovies.imdb_rating.max())
+
+
+
